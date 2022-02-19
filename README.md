b8affca0dabf214bbee2f0e0b735984b88672ac4
chandrasekhar09 <chandrasekhar.kommi@outlook.com>
2022-02-19 22:04:25 +0530

    initial setup

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Bootstap
+# bootstrap
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.2.17.
 
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/bootstap'),
+      dir: require('path').join(__dirname, './coverage/bootstrap'),
       subdir: '.',
       reporters: [
         { type: 'html' },
